7b8ef2c2c6af56e359c7c42f70750c00131e8d35
GustavoArcoverde <garcoverde3@gmail.com>
2022-05-25 20:14:20 -0300

    add arquivo de melhor farm

---
+++ responde_pergunta_melhor_farm_de_cada_ano.sql
@@ -0,0 +1,11 @@
+select player as jogador, CSPM as farm_por_minuto from `worlds 2021 main event - player stats` order by CSPM desc limit 1;
+select player as jogador, CSPM as farm_por_minuto from `worlds 2020 main event - player stats` order by CSPM desc limit 1;
+select player as jogador, CSPM as farm_por_minuto from `worlds 2019 main event - player stats` order by CSPM desc limit 1;
+select player as jogador, CSPM as farm_por_minuto from `worlds 2018 main event - player stats` order by CSPM desc limit 1;
+select player as jogador, CSPM as farm_por_minuto from `worlds 2017 main event - player stats` order by CSPM desc limit 1;
+select player as jogador, CSPM as farm_por_minuto from `worlds 2016 main event - player stats` order by CSPM desc limit 1;
+select player as jogador, CSPM as farm_por_minuto from `worlds 2015 main event - player stats` order by CSPM desc limit 1;
+select player as jogador, CSPM as farm_por_minuto from `worlds 2014 main event - player stats` order by CSPM desc limit 1;
+select player as jogador, CSPM as farm_por_minuto from `worlds season 1 - player stats` order by CSPM desc limit 1;
+select player as jogador, CSPM as farm_por_minuto from `worlds season 2 - player stats` order by CSPM desc limit 1;
+select player as jogador, CSPM as farm_por_minuto from `worlds season 3 - player stats` order by CSPM desc limit 1;
