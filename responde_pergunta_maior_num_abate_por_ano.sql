b14bdd05be02334a86834534e99c7dc030f81bb7
GustavoArcoverde <garcoverde3@gmail.com>
2022-05-26 15:03:25 -0300

    responde pergunta de maior num de abate

---
+++ responde_pergunta_maior_num_abate_por_ano.sql
@@ -0,0 +1,11 @@
+select player as jogador, K as num_abates from `worlds 2021 main event - player stats` order by K desc limit 1;
+select player as jogador, K as num_mortes from `worlds 2020 main event - player stats` order by K desc limit 1;
+select player as jogador, K as num_mortes from `worlds 2019 main event - player stats` order by K desc limit 1;
+select player as jogador, K as num_mortes from `worlds 2018 main event - player stats` order by K desc limit 1;
+select player as jogador, K as num_mortes from `worlds 2017 main event - player stats` order by K desc limit 1;
+select player as jogador, K as num_mortes from `worlds 2016 main event - player stats` order by K desc limit 1;
+select player as jogador, K as num_mortes from `worlds 2015 main event - player stats` order by K desc limit 1;
+select player as jogador, K as num_mortes from `worlds 2014 main event - player stats` order by K desc limit 1;
+select player as jogador, K as num_mortes from `worlds season 1 - player stats` order by K desc limit 1;
+select player as jogador, K as num_mortes from `worlds season 2 - player stats` order by K desc limit 1;
+select player as jogador, K as num_mortes from `worlds season 3 - player stats` order by K desc limit 1;
