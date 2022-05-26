70ef7a093d8f347d95b4e67f74d0bbf7c3429c8c
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-05-26 17:58:49 -0300

    consulta melhor player de cada mundial

--- sql_jess.sql
+++ sql_jess.sql
@@ -2,3 +2,34 @@ SELECT * FROM leagueoflegends.worldsseason1;
 
 #Primeira consulta no banco, melhor player de 2011 baseado no KDA.
 select Player, Pos, K,D,A,KDA,KP from leagueoflegends.worldsseason1 order by KDA desc;
+
+#Melhor player de 2012 baseado no KDA.
+select Player, Pos, K,D,A,KDA,KP from leagueoflegends.worldsseason2 order by KDA desc;
+
+#Melhor player de 2013 baseado no KDA.
+select Player, Pos, K,D,A,KDA,KP from leagueoflegends.worldsseason3 order by KDA desc;
+
+#Melhor player de 2014 baseado no KDA.
+select Player, Pos, K,D,A,KDA,KP from leagueoflegends.worlds2014 order by KDA desc;
+
+#Melhor player de 2015 baseado no KDA.
+select Player, Pos, K,D,A,KDA,KP from leagueoflegends.worlds2015 order by KDA desc;
+
+#Melhor player de 2016 baseado no KDA.
+select Player, Pos, K,D,A,KDA,KP from leagueoflegends.worlds2016 order by KDA desc;
+
+#Melhor player de 2017 baseado no KDA.
+select Player, Pos, K,D,A,KDA,KP from leagueoflegends.worlds2017 order by KDA desc;
+
+#Melhor player de 2018 baseado no KDA.
+select Player, Pos, K,D,A,KDA,KP from leagueoflegends.worlds2018 order by KDA desc;
+
+#Melhor player de 2019 baseado no KDA.
+select Player, Pos, K,D,A,KDA,KP from leagueoflegends.worlds2019 order by KDA desc;
+
+#Melhor player de 2020 baseado no KDA.
+select Player, Pos, K,D,A,KDA,KP from leagueoflegends.worlds2020 order by KDA desc;
+
+#Melhor player de 2021 baseado no KDA.
+select Player, Pos, K,D,A,KDA,KP from leagueoflegends.worlds2021 order by KDA desc;
+
